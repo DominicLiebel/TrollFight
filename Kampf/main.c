16a77437667bfaa507dfd7ad18e2f9a5f98578b9
Dominic Liebel <dominicliebel@Dominics-MBP.fritz.box>
2016-07-31 14:03:59 +0200

    Troll Fight

--- Kampf/main.c
+++ Kampf/main.c
@@ -1,15 +1,49 @@
 //
 //  main.c
-//  Kampf
+//  SortArray
 //
-//  Created by Dominic Liebel on 31.07.16.
-//  Copyright © 2016 Pascal Liebel. All rights reserved.
+//  Created by Dominic Liebel on 30.07.16.
+//  Copyright © 2016 Dominic Liebel. All rights reserved.
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int erGreiftAn = 0;
+    int sieGreifenAn = 0;
+    srand(time(NULL));
+    int r = rand() % 10;
+    char* gegner = "\"Ich bin der Fette Troll. Ich mach dich kaputt.\"\n";
+    
+    char KampfenOderRennen;
+    
+    int leben = 100;
+    printf("Sie haben noch %i Leben.\n", leben);
+    
+    printf("Vor ihnen taucht ein Gegner auf.\n\n");
+    printf("%s", gegner);
+    
+    
+    printf("Wollen Sie wegrennen oder direkt kämpfen gegen den Gegner.");
+    scanf("%c", &KampfenOderRennen);
+    if(KampfenOderRennen == 'r' && r < 5) {
+        printf("Sie sind erfolgreich geflohen!\n");
+    } else if (KampfenOderRennen == 'k'){
+        printf("Sie sind nicht geflohen, Sie greifen den Troll an!\n");
+        int sieGreifenAn = 1;
+    } else {
+                printf("Sie sind nicht geflohen, der Troll greift Sie an!\n");
+        int erGreiftAn = 1;
+    }
+    
+    
+    
+    
+    
+    
     return 0;
 }
