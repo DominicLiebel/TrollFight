011254bca8d20c54157e54d51a38178c78cc00d2
Dominic Liebel <dominicliebel@dominics-mbp.fritz.box>
2016-07-31 13:57:26 +0200

    Initial Commit

---
+++ Kampf/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Kampf
+//
+//  Created by Dominic Liebel on 31.07.16.
+//  Copyright © 2016 Pascal Liebel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
