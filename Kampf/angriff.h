92569fc851ea521887dcaa9c41d892014d3f8f08
Dominic Liebel <dominicliebel@Dominics-MBP.fritz.box>
2016-08-02 18:51:00 +0200

    Updated troll fight, added lots of functions

---
+++ Kampf/angriff.h
@@ -0,0 +1,13 @@
+//
+//  angriff.h
+//  Kampf
+//
+//  Created by Dominic Liebel on 02.08.16.
+//  Copyright © 2016 Pascal Liebel. All rights reserved.
+//
+
+#ifndef angriff_h
+#define angriff_h
+
+
+#endif /* angriff_h */
--- Kampf/main.c
+++ Kampf/main.c
@@ -13,7 +13,12 @@
 
 int main(int argc, const char * argv[]) {
     
+    
+    
+    
+    int geflohen = 0;
     int erGreiftAn = 0;
+    int gegnerLeben = 42;
     int sieGreifenAn = 0;
     srand(time(NULL));
     int r = rand() % 10;
@@ -30,18 +35,50 @@ int main(int argc, const char * argv[]) {
     
     printf("Wollen Sie wegrennen oder direkt kämpfen gegen den Gegner.");
     scanf("%c", &KampfenOderRennen);
-    if(KampfenOderRennen == 'r' && r < 5) {
+    if(KampfenOderRennen == 'f' && r < 5) {
         printf("Sie sind erfolgreich geflohen!\n");
+        geflohen +=1;
     } else if (KampfenOderRennen == 'k'){
         printf("Sie sind nicht geflohen, Sie greifen den Troll an!\n");
-        int sieGreifenAn = 1;
+        printf("Der Gegner (Troll) get %i Leben.\n", gegnerLeben);
+        sieGreifenAn = 1;
+        
     } else {
                 printf("Sie sind nicht geflohen, der Troll greift Sie an!\n");
-        int erGreiftAn = 1;
+                printf("Der Gegner (Troll) get %i Leben.\n", gegnerLeben);
+        erGreiftAn = 1;
     }
     
+
     
     
+    
+    srand(time(NULL));
+        while(leben > 0 && gegnerLeben > 0) {
+    while(leben > 0 && gegnerLeben > 0 && erGreiftAn == 1) {
+        int randomNumber = rand() % 12;
+        printf("\nDer Gegner hat ihnen %i Leben abgezogen!",randomNumber);
+                       leben -= randomNumber;
+        printf("\nSie haben noch %i Leben!\n", leben);
+            
+            erGreiftAn -= 1;
+            sieGreifenAn += 1;
+    }
+            while(leben > 0 && gegnerLeben > 0 && sieGreifenAn == 1) {
+                    int randomNumber2 = rand() % 5;
+            printf("\nSie haben dem Gegner %i Leben abgezogen!", randomNumber2);
+                        gegnerLeben -= randomNumber2;
+                
+            printf("\nDer Gegner hat noch %i Leben!\n", gegnerLeben);
+           
+            erGreiftAn += 1;
+            sieGreifenAn -= 1;
+            }
+    
+        }
+    if(leben > 0 && geflohen == 0) {
+        printf("\nSie haben den Kampf gewonnen! \n Sie haben noch %i Leben\n", leben);
+    }
     
     
     
